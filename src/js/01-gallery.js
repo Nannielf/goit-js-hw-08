198e99acaa41a1633ea347f662b37163357ac78b
Nannielf <nannielf.no@gmail.com>
2023-10-23 15:31:49 +0300

    dz1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,18 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+const gallery = document.querySelector('.gallery');
+function markup(arr){
+    return arr.map(({preview, original, description}) => `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}" onclick="event.preventDefault()">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`).join('')
+};
+gallery.insertAdjacentHTML('afterbegin', markup(galleryItems));
+gallery.style = 'list-style-type: none';
+new SimpleLightbox('.gallery__item a', { captionsData: 'alt', captionDelay:250 });
 console.log(galleryItems);
