d8561bc1d7f8f80e9cd7d639780d095334a24c73
Nannielf <nannielf.no@gmail.com>
2023-10-24 03:58:45 +0300

    dz2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    const KEY_TIME = 'videoplayer-current-time';
+    
+    player.on('timeupdate', throttle(upTime, 1000));
+
+    function upTime({ seconds }) {
+      localStorage.setItem(KEY_TIME, seconds);
+    };
+    window.addEventListener("load", 
+    player.setCurrentTime(localStorage.getItem(KEY_TIME) || 0));
