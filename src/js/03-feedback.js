144b00e2d648729f15130624698070666382dfc9
Nannielf <nannielf.no@gmail.com>
2023-10-24 04:38:18 +0300

    dz3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const feedback = document.querySelector('.feedback-form');
+const KEY_OBJECT = 'feedback-form-state';
+let objectForm = {};
+
+feedback.addEventListener('input', throttle(handlerInput, 500));
+
+feedback.addEventListener('submit', handlerSubmit);
+
+function handlerInput(event) {
+    objectForm = JSON.parse(localStorage.getItem(KEY_OBJECT)) || {};
+
+    objectForm[event.target.name] = event.target.value;
+
+  localStorage.setItem(KEY_OBJECT, JSON.stringify(objectForm));
+}
+
+function handlerSubmit(event) {
+  event.preventDefault();
+  if (!event.target.email.value || !event.target.message.value) {
+    alert('Заполните поля формы!');
+    return;
+  }
+
+  event.target.reset();
+  console.log(objectForm);
+  localStorage.removeItem(KEY_OBJECT);
+}
